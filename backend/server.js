ef953227006747a939be264c5c719189be56d318
Kuldeep Singh <kuldipsingh1620@gmail.com>
2025-01-08 20:20:01 +0530

    control

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
+import path from "path"
 
 
 //app config
 const app = express()
 const port = 4000
 
+const __dirname = path.resolve();
 //middleware
 app.use(express.json())
 app.use(cors())
@@ -25,6 +27,14 @@ app.use("/images", express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/admin/dist")));
+app.get("*", (req, res) => {
+     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+     res.sendFile(path.resolve(__dirname, "admin", "dist", "index.html"));
+
+});
 
 app.get("/",(req,res)=>{
     res.send("API Working")
